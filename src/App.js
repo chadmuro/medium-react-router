a0369205f3afcbc52df92dd6f1548aa6fb49af02
chadmuro <63533886+chadmuro@users.noreply.github.com>
2021-01-12 11:37:15 +0900

    add Switch and Route to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>App</h1>
+			<Switch>
+				<Route path="/about" component={About} />
+				<Route path="/" component={Home} />
+			</Switch>
 		</div>
 	);
 }
